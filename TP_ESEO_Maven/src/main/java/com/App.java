35680681958e33048fc07ab2df8ec4766207cdb1
AlainDF <alain.diasfernandes@reseau.eseo.fr>
2018-11-28 14:39:35 +0100

    Try remote build

--- TP_ESEO_Maven/src/main/java/com/App.java
+++ TP_ESEO_Maven/src/main/java/com/App.java
@@ -1,11 +1,9 @@
 package com;
 
-import java.sql.*;
-
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "!! dlrow olleh" );
     }
 }
